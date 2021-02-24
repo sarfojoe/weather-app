077253631e992ef8259c3ec77c3e89ae697e9257
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2021-02-24 10:39:30 +0200

    functions updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-//import { ScaleLoader } from "react-spinners";
+import { getWeatherData } from "./data/weatherapi";
+import { ScaleLoader } from "react-spinners";
 
 function App() {
   const [weatherdata, setWeatherData] = useState(null);
-  const [city, setCity] = useState("Lahore");
+  const [city, setCity] = useState("Helsinki");
   const [loading, setLoading] = useState(false);
 
   const getData = async () => {
@@ -18,7 +19,14 @@ function App() {
       setLoading(false);
     }
   };
-
+  const override = `
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+  `;
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="App">
       <div className="card">
@@ -36,6 +44,52 @@ function App() {
             Search
           </button>
         </div>
+        {loading ? (
+          <div className="loader-container">
+            <ScaleLoader
+              css={override}
+              size={200}
+              color={"#fff"}
+              loading={loading}
+            />
+          </div>
+        ) : (
+          <>
+            {weatherdata !== null ? (
+              <div className="main-container">
+                <h4>Live Weather Condition</h4>
+                <div className="weather-icon">
+                  <img
+                    src={`http://openweathermap.org/img/w/${weatherdata.weather[0].icon}.png`}
+                    alt="imgicon"
+                  />
+                </div>
+                <h3>{weatherdata.weather[0].main}</h3>
+                <div className="temprature">
+                  <h1>
+                    {parseFloat(weatherdata.main.temp - 273.15).toFixed(1)}
+                    &deg;C
+                  </h1>
+                </div>
+                <div className="location">
+                  <h3>
+                    <i className="fa fa-street-view"></i>
+                    {weatherdata.name} | {weatherdata.sys.country}
+                  </h3>
+                </div>
+                <div className="temprature-range">
+                  <h6>
+                    Min:
+                    {parseFloat(weatherdata.main.temp_min - 273.15).toFixed(1)}
+                    &deg;C || Max:
+                    {parseFloat(weatherdata.main.temp_max - 273.15).toFixed(1)}
+                    &deg;C || Humidity: {weatherdata.main.humidity}%
+                  </h6>
+                </div>
+              </div>
+            ) : null}
+          </>
+        )}
       </div>
     </div>
   );
