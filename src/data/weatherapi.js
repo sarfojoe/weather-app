de86fe640cc8746cee9d1ce9042df67a0feb9b2a
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2021-02-25 10:42:16 +0200

    env added

--- src/data/weatherapi.js
+++ src/data/weatherapi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
-const apiKey = "";
+const apiKey = "f5baf4a4ecec8afcb8aa07f93da4763b";
 
 export const getWeatherData = async (cityname) => {
   try {
